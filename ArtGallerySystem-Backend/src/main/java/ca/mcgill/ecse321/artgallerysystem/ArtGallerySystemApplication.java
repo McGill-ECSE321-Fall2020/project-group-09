62d20e60604774ca700ecf30cb7cd2e18c9c744c
Duan Linpei <dlp1654978696@gmail.com>
2020-10-03 11:33:52 -0400

    add import in application

--- ArtGallerySystem-Backend/src/main/java/ca/mcgill/ecse321/artgallerysystem/ArtGallerySystemApplication.java
+++ ArtGallerySystem-Backend/src/main/java/ca/mcgill/ecse321/artgallerysystem/ArtGallerySystemApplication.java
@@ -1,16 +1,21 @@
 package ca.mcgill.ecse321.artgallerysystem;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @SpringBootApplication
-public class ArtGallerySystemApplication{
-public static void main(String[] args) {
-SpringApplication.run(ArtGallerySystemApplication.class, args);
-     }
+public class ArtGallerySystemApplication {
 
-@RequestMapping("/")
-public String greeting ()
-{
-return "Hello world!";
-     }
+  public static void main(String[] args) {
+    SpringApplication.run(ArtGallerySystemApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
+
 }
