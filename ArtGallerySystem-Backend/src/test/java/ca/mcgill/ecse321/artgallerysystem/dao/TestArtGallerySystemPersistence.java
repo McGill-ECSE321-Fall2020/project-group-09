fe862f2bf735db4255c46981d8bf9b6dc2b417b4
Tzy28 <54202935+Tzy28@users.noreply.github.com>
2020-10-06 12:46:55 +0800

    Testing suite update

--- ArtGallerySystem-Backend/src/test/java/ca/mcgill/ecse321/artgallerysystem/dao/TestArtGallerySystemPersistence.java
+++ ArtGallerySystem-Backend/src/test/java/ca/mcgill/ecse321/artgallerysystem/dao/TestArtGallerySystemPersistence.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Month;
 
+import jdk.nashorn.internal.runtime.logging.DebugLogger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ca.mcgill.ecse321.artgallerysystem.model.*;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class TestArtGallerySystemPersistence {
@@ -34,6 +36,8 @@ public class TestArtGallerySystemPersistence {
 	private PaymentRepository paymentRepository;
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private ArtGallerySystemRepository artGallerySystemRepository;
 	@AfterEach
 	public void clearDatabase() {
 		addressRepository.deleteAll();
@@ -44,22 +48,142 @@ public class TestArtGallerySystemPersistence {
 		orderRepository.deleteAll();
 		paymentRepository.deleteAll();
 		userRepository.deleteAll();
+		artGallerySystemRepository.deleteAll();
 	}
+
+
+	//Test System
+	@Test
+	public void testPersistAndLoadSystem(){
+
+	}
+
+
+
+
+	//Test User
 	@Test
 	public void testPersistAndLoadUser() {
 		String name = "TestUser";
-		// First example for object save/load
 		User user = new User();
-		// First example for attribute save/load
 		user.setName(name);
 		userRepository.save(user);
-
 		user = null;
 
 		user = userRepository.findUserByName(name);
-		assertNotNull(user);
+		assertNotNull(user,"failed adding user to repository");
 		assertEquals(name, user.getName());
+
 	}
-	
+
+
+	//Test Address
+	@Test
+	public void testPersistAndLoadAddress(){
+		String location = "TestAddress";
+		Address address = new Address();
+		address.setAddressId(location);
+		addressRepository.save(address);
+		address = null;
+
+		address = addressRepository.findAddressById(location);
+		assertNotNull(address,"failed adding address to repository");
+		assertEquals(location,address.getAddressId());
+	}
+
+	//Test Artist
+	@Test
+	public void testPersistAndLoadArtisit() {
+		String aid = "TestArtist";
+		Artist artist = new Artist();
+		artist.setArtistId(aid);
+		artistRepository.save(artist);
+		artist = null;
+
+		artist = artistRepository.findArtistById(aid);
+		assertNotNull(artist,"failed adding address to repository");
+		assertEquals(aid,artist.getArtistId());
+	}
+
+	//Test Customer
+	@Test
+	public void testPersistAndLoadCustomer(){
+		String cid = "TestCustomer";
+		Customer customer = new Customer();
+		customer.setCustomerId(cid);
+		customerRepository.save(customer);
+		customer = null;
+
+		customer = customerRepository.findCustomerById(cid);
+		assertNotNull(customer,"failed adding customer to repository");
+		assertEquals(cid,customer.getCustomerId());
+	}
+
+	//Test Artpiece
+	@Test
+	public void testPersistAndLoadArtPiece(){
+		String apid = "TestArtPiece";
+		ArtPiece artPiece = new ArtPiece();
+		artPiece. setArtPieceId(apid);
+		artpieceRepository.save(artPiece);
+		artPiece = null;
+
+		artPiece = artpieceRepository.findArtPieceById(apid);
+		assertNotNull(artPiece,"failed adding artPiece to repository");
+		assertEquals(apid,artPiece.getArtPieceId());
+	}
+
+	//Test Order
+	@Test
+	public void testPersistAndLoadOrder(){
+		String oid = "TestOrder";
+		Order order = new Order();
+		order.setOrderId(oid);
+		orderRepository.save(order);
+		order = null;
+
+		order = orderRepository.findOrderById(oid);
+		assertNotNull(order,"failed adding order to repository");
+		assertEquals(oid,order.getOrderId());
+	}
+
+	//TestPayment
+	@Test
+	public void testPersistAndLoadPayment(){
+		String pid = "TestPayment";
+		Payment payment = new Payment();
+		payment.setPaymentId(pid);
+		paymentRepository.save(payment);
+		payment = null;
+
+		payment = paymentRepository.findPaymentById(pid);
+		assertNotNull(payment,"failed adding payment to repository");
+		assertEquals(pid,payment.getPaymentId());
+	}
+
+	//Test Delivery
+	@Test
+	public void testPersistAndLoadDelivery(){
+		String pdid = "TestParcel";
+		String isid = "TestPickUp";
+
+		Delivery parcelDelivery = new ParcelDelivery();
+		Delivery inStorePickUp = new InStorePickUp();
+
+
+		parcelDelivery.setDeliveryId(pdid);
+		inStorePickUp.setDeliveryId(isid);
+		deliveryRepository.save(parcelDelivery);
+		deliveryRepository.save(inStorePickUp);
+		parcelDelivery = null;
+		inStorePickUp = null;
+
+		parcelDelivery = deliveryRepository.findDeliveryById(pdid);
+		inStorePickUp = deliveryRepository.findDeliveryById(isid);
+		assertNotNull(parcelDelivery,"failed adding parcel delivery to repository");
+		assertEquals(pdid,parcelDelivery.getDeliveryId());
+		assertNotNull(inStorePickUp,"failed adding instore pick up to repository");
+		assertEquals(isid,inStorePickUp.getDeliveryId());
+	}
 
 }
