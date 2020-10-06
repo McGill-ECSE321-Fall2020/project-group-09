477b6f8e7772d06ee23ae455fcf332796b9ba46e
zhekai-jiang <zhekai.jiang@mail.mcgill.ca>
2020-10-06 14:35:04 +0800

    Add a missing repository class

---
+++ ArtGallerySystem-Backend/src/main/java/ca/mcgill/ecse321/artgallerysystem/dao/UserRoleRepository.java
@@ -0,0 +1,10 @@
+package ca.mcgill.ecse321.artgallerysystem.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.artgallerysystem.model.UserRole;
+
+
+public interface UserRoleRepository extends CrudRepository <UserRole, String>   {
+	public UserRole findUserRoleById(String id);
+}
