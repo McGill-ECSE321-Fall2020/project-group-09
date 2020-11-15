073e55ba68b5e1aac114883635b4cb8a8009c008
AmeliaWen <cuiwen423@gmail.com>
2020-11-15 18:30:38 -0500

    trying

--- ArtGallerySystem-Frontend/build/dev-server.js
+++ ArtGallerySystem-Frontend/build/dev-server.js
@@ -15,7 +15,8 @@ var webpackConfig = (process.env.NODE_ENV === 'testing' || process.env.NODE_ENV 
   : require('./webpack.dev.conf')
 
 // default port where dev server listens for incoming traffic
-var port = process.env.PORT || config.dev.port
+//var port = process.env.PORT || config.dev.port
+var port = config.dev.port
 // automatically open browser, if not set will be false
 var autoOpenBrowser = !!config.dev.autoOpenBrowser
 // Define HTTP proxies to your custom API backend
