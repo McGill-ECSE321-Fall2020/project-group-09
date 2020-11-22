bba5207ef59334734fe87ba8d240d2017331d162
zhekai-jiang <zhekai.jiang@mail.mcgill.ca>
2020-11-22 21:57:07 +0800

    Fix url bug in AddArtPiece

--- ArtGallerySystem-Frontend/src/components/AddArtPiece.vue
+++ ArtGallerySystem-Frontend/src/components/AddArtPiece.vue
@@ -91,8 +91,8 @@ let AXIOS = axios.create({
   baseURL: backendUrl,
   headers: { 'Access-Control-Allow-Origin': frontendUrl }
 });*/
-var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+var frontendUrl = frontendConfigurer()
+var backendUrl = backendConfigurer()
 
 var AXIOS = axios.create({
   baseURL: backendUrl,
